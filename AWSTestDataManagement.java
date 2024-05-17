2c67c5b87bed699662f2772d6dccd35edb36c981
manishdube <manish.dube@gmail.com>
2024-05-16 21:21:26 -0400

    Update AWSTestDataManagement.java

--- AWSTestDataManagement.java
+++ AWSTestDataManagement.java
@@ -123,4 +123,77 @@ public class AWSTestDataManagement {
 
             Map<String, software.amazon.awssdk.services.dynamodb.model.AttributeValueUpdate> attributeUpdates = new HashMap<>();
             updatedValues.forEach((k, v) -> attributeUpdates.put(k, software.amazon.awssdk.services.dynamodb.model.AttributeValueUpdate.builder()
-                    .value(software.amazon.awssdk.services.dynamodb.model.Attribute
+                    .value(software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().s(v.toString()).build())
+                    .action(software.amazon.awssdk.services.dynamodb.model.AttributeAction.PUT)
+                    .build()));
+
+            UpdateItemRequest request = UpdateItemRequest.builder()
+                    .tableName(tableName)
+                    .key(key)
+                    .attributeUpdates(attributeUpdates)
+                    .build();
+
+            dynamoDbClient.updateItem(request);
+            System.out.println("Item updated successfully");
+        } catch (DynamoDbException e) {
+            System.err.println("Unable to update item: ");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Method to delete an item from a DynamoDB table
+     * @param tableName Name of the table
+     * @param keyName Key name of the item
+     * @param keyValue Key value of the item
+     */
+    public void deleteItem(String tableName, String keyName, Object keyValue) {
+        try {
+            System.out.println("Deleting item from table " + tableName);
+            Map<String, software.amazon.awssdk.services.dynamodb.model.AttributeValue> key = new HashMap<>();
+            key.put(keyName, software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().s(keyValue.toString()).build());
+
+            DeleteItemRequest request = DeleteItemRequest.builder()
+                    .tableName(tableName)
+                    .key(key)
+                    .build();
+
+            dynamoDbClient.deleteItem(request);
+            System.out.println("Item deleted successfully");
+        } catch (DynamoDbException e) {
+            System.err.println("Unable to delete item: ");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example usage:
+        createTable("TestTable", "TestKey");
+
+        AWSTestDataManagement manager = new AWSTestDataManagement();
+
+        // Example item
+        Map<String, Object> item = new HashMap<>();
+        item.put("ID", "123");
+        item.put("Name", "Test Item");
+        item.put("Value", "This is a test");
+
+        // Put item
+        manager.putItem("TestTable", item);
+
+        // Get item
+        manager.getItem("TestTable", "ID", "123");
+
+        // Update item
+        Map<String, Object> updatedValues = new HashMap<>();
+        updatedValues.put("Name", "Updated Test Item");
+        updatedValues.put("Value", "This is an updated test");
+        manager.updateItem("TestTable", "ID", "123", updatedValues);
+
+        // Get updated item
+        manager.getItem("TestTable", "ID", "123");
+
+        // Delete item
+        manager.deleteItem("TestTable", "ID", "123");
+    }
+}
