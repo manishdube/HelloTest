82bf7dfe4b6143832b0f48ab4eeec3218aca0eaf
manishdube <manish.dube@gmail.com>
2024-05-22 22:19:49 -0400

    Create ttemp.java

---
+++ ttemp.java
@@ -0,0 +1,10 @@
+
+Join the meeting now<https://teams.microsoft.com/l/meetup-join/19%3ameeting_MDJkZjhhN2EtYzBhZS00Y2NlLWJkMmUtYTgzOWRhMGJkMTRk%40thread.v2/0?context=%7b%22Tid%22%3a%228ac76c91-e7f1-41ff-a89c-3553b2da2c17%22%2c%22Oid%22%3a%22c8ee2677-e3f5-4d17-8e59-8b9e32b9e238%22%7d>
+Meeting ID: 279 054 699 684
+Passcode: 8vzHse
+________________________________
+Join on a video conferencing device
+Tenant key: wk@m.webex.com
+Video ID: 113 477 035 2
+More info<https://www.webex.com/msteams?confid=1134770352&tenantkey=wk&domain=m.webex.com>
+For organizers: Meeting options<https://teams.microsoft.com/meetingOptions/?organizerId=c8ee2677-e3f5-4d17-8e59-8b9e32b9e238&tenantId=8ac76c91-e7f1-41ff-a89c-3553b2da2c17&threadId=19_meeting_MDJkZjhhN2EtYzBhZS00Y2NlLWJkMmUtYTgzOWRhMGJkMTRk@thread.v2&messageId=0&language=en-US>
