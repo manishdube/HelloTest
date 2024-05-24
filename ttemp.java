6640faae6e37aa79ab6829b3bdbdc59f75ef69cf
manishdube <manish.dube@gmail.com>
2024-05-24 14:22:06 -0400

    Update ttemp.java

--- ttemp.java
+++ ttemp.java
@@ -8,3 +8,19 @@ Tenant key: wk@m.webex.com
 Video ID: 113 477 035 2
 More info<https://www.webex.com/msteams?confid=1134770352&tenantkey=wk&domain=m.webex.com>
 For organizers: Meeting options<https://teams.microsoft.com/meetingOptions/?organizerId=c8ee2677-e3f5-4d17-8e59-8b9e32b9e238&tenantId=8ac76c91-e7f1-41ff-a89c-3553b2da2c17&threadId=19_meeting_MDJkZjhhN2EtYzBhZS00Y2NlLWJkMmUtYTgzOWRhMGJkMTRk@thread.v2&messageId=0&language=en-US>
+
+
+
+
+________________________________________________________________________________
+Microsoft Teams Need help?<https://aka.ms/JoinTeamsMeeting?omkt=en-US>
+Join the meeting now<https://teams.microsoft.com/l/meetup-join/19%3ameeting_YWY1NGY1YjYtNTk2Zi00N2Y4LWIyNWItNmY0OWYzNWQ1YjIy%40thread.v2/0?context=%7b%22Tid%22%3a%228ac76c91-e7f1-41ff-a89c-3553b2da2c17%22%2c%22Oid%22%3a%22c8ee2677-e3f5-4d17-8e59-8b9e32b9e238%22%7d>
+Meeting ID: 273 962 892 96
+Passcode: i9aBrb
+________________________________
+Join on a video conferencing device
+Tenant key: wk@m.webex.com
+Video ID: 118 943 125 7
+More info<https://www.webex.com/msteams?confid=1189431257&tenantkey=wk&domain=m.webex.com>
+For organizers: Meeting options<https://teams.microsoft.com/meetingOptions/?organizerId=c8ee2677-e3f5-4d17-8e59-8b9e32b9e238&tenantId=8ac76c91-e7f1-41ff-a89c-3553b2da2c17&threadId=19_meeting_YWY1NGY1YjYtNTk2Zi00N2Y4LWIyNWItNmY0OWYzNWQ1YjIy@thread.v2&messageId=0&language=en-US>
+________________________________________________________________________________
