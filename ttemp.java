f69cf99fccc3167b7bfce9cb3bb622e2e9d48727
manishdube <manish.dube@gmail.com>
2024-05-29 19:56:51 -0400

    Update ttemp.java

--- ttemp.java
+++ ttemp.java
@@ -1,26 +1,35 @@
 
-Join the meeting now<https://teams.microsoft.com/l/meetup-join/19%3ameeting_MDJkZjhhN2EtYzBhZS00Y2NlLWJkMmUtYTgzOWRhMGJkMTRk%40thread.v2/0?context=%7b%22Tid%22%3a%228ac76c91-e7f1-41ff-a89c-3553b2da2c17%22%2c%22Oid%22%3a%22c8ee2677-e3f5-4d17-8e59-8b9e32b9e238%22%7d>
-Meeting ID: 279 054 699 684
-Passcode: 8vzHse
-________________________________
-Join on a video conferencing device
-Tenant key: wk@m.webex.com
-Video ID: 113 477 035 2
-More info<https://www.webex.com/msteams?confid=1134770352&tenantkey=wk&domain=m.webex.com>
-For organizers: Meeting options<https://teams.microsoft.com/meetingOptions/?organizerId=c8ee2677-e3f5-4d17-8e59-8b9e32b9e238&tenantId=8ac76c91-e7f1-41ff-a89c-3553b2da2c17&threadId=19_meeting_MDJkZjhhN2EtYzBhZS00Y2NlLWJkMmUtYTgzOWRhMGJkMTRk@thread.v2&messageId=0&language=en-US>
+Endorsements for Myself:
 
+Dr. Oliver Degnan
+Position: Supervisor (CIO)
+LinkedIn Profile: www.linkedin.com/in/drdegnan/
+Contact: 1 (414) 207-2345
+Special Commendation: "Manish's exceptional contribution and dedication are instrumental in resolving THE company-wide challenge."Jagan Jevaji
 
+Position: Peer (Architect / Director)
+Contact: +1 (734) 846-1234
+Special Commendation: "We are lucky to have a leader like Manish."A Gehani
 
+Position: Supervisor (VP)
+LinkedIn Profile: www.linkedin.com/in/ajaygehanisprofile/
+Contact: 1 (678) 699-8901
+Notable Remark: "Manish is a dynamite problem solver."Sam Esfahani
 
-________________________________________________________________________________
-Microsoft Teams Need help?<https://aka.ms/JoinTeamsMeeting?omkt=en-US>
-Join the meeting now<https://teams.microsoft.com/l/meetup-join/19%3ameeting_YWY1NGY1YjYtNTk2Zi00N2Y4LWIyNWItNmY0OWYzNWQ1YjIy%40thread.v2/0?context=%7b%22Tid%22%3a%228ac76c91-e7f1-41ff-a89c-3553b2da2c17%22%2c%22Oid%22%3a%22c8ee2677-e3f5-4d17-8e59-8b9e32b9e238%22%7d>
-Meeting ID: 273 962 892 96
-Passcode: i9aBrb
-________________________________
-Join on a video conferencing device
-Tenant key: wk@m.webex.com
-Video ID: 118 943 125 7
-More info<https://www.webex.com/msteams?confid=1189431257&tenantkey=wk&domain=m.webex.com>
-For organizers: Meeting options<https://teams.microsoft.com/meetingOptions/?organizerId=c8ee2677-e3f5-4d17-8e59-8b9e32b9e238&tenantId=8ac76c91-e7f1-41ff-a89c-3553b2da2c17&threadId=19_meeting_YWY1NGY1YjYtNTk2Zi00N2Y4LWIyNWItNmY0OWYzNWQ1YjIy@thread.v2&messageId=0&language=en-US>
-________________________________________________________________________________
+Position: Supervisor (CIO)
+LinkedIn Profile: www.linkedin.com/in/sam-esfahani-ab49a03/
+Notable Remark: "Manish has shown remarkable testing leadership. Never seen anything like this ..."Ed Robinson
+
+Position: Supervisor (COO)
+LinkedIn Profile: www.linkedin.com/in/edwardlrobinson/
+Notable Remark: "Manish is the definition of Testing."Greg Chi
+
+Position: Supervisor (CIO)
+LinkedIn Profile: www.linkedin.com/in/gregory-chi/
+Notable Remark: "Manish has shown tremendous testing leadership under chaos and remote work."Augusto Garcia
+
+Test Lead
+Special Commendation: "My memories of working under Manish were the best of my working career."Should you require any additional information or wish to contact any of my references directly, please let me know.Best regards,
+Manish
+
+  
