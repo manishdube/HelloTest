0eabb7f5122f2327587b7ef81898a64a26302559
manishdube <manish.dube@gmail.com>
2024-06-20 15:59:32 -0400

    Update releaseNotes.java

--- releaseNotes.java
+++ releaseNotes.java
@@ -1,3 +1,81 @@
+
+Category,Area,Feature,Version
+Zephyr Integration,Zephyr Integration,Zephyr Integration (GBSCAF-4),1.1
+Schema Validation,Schema Validation,Schema Validation (GBSCAFDEV-17),1.1
+Test Data Support,Test Data Support,Test Data Support with CSV (GBSCAFDEV-12),1.1
+Video Recording,Video Recording,Video Recording (GBSCAFDEV-134),1.1
+Dependency Management,Dependency Management,Nexus Dependency Management (GBSCAFADT-39),1.1
+Splunk Integration,Splunk Integration,Splunk Integration (GBSW-139533),1.1
+Test Automation,Test Automation Enhancements,Re-run Failed Tests (GBSCAFADT-50),1.1
+Automation Framework,Automation Framework Upgrade,Migration of Selenium 3 to Selenium 4 (GBSCAFADT-254),1.2
+Zephyr Integration,Zephyr Integration,Zephyr Integration and Test Reporting Enhancement,1.2
+Test Automation,Test Automation Enhancements,Automation and Testing Framework Improvements,1.2
+Dependency Management,Dependency Management,Dependency Management and Documentation Updates,1.2
+Mobile Testing,Mobile Testing,Mobile Testing and Environment Integration,1.2
+Bug Fixes,Bug Fixes,Bug Fixes and Stability Improvements: Chrome driver,1.2
+CI/CD Integration,CI/CD Integration,GitLab Runners set for Non-Prod AWS instance,1.3
+Zephyr Integration,Zephyr Integration,Zephyr Enhancement: Improved integration with Zephyr for better test management and reporting capabilities,1.3
+Proxy Configuration,Proxy Configuration,Proxy Configuration for Zephyr,1.3
+Zephyr Integration,Zephyr Enhancements,Zephyr Enhancements (Folder Creation & Search API Update),1.3
+Zephyr Integration,Zephyr Enhancements,Zephyr Logic Change for Attachments,1.3
+Zephyr Integration,Zephyr Enhancements,Zephyr Enhancements - Clean up and Create Test Case JSON Parameterization,1.3
+Dependency Management,Dependency Management,POM Dependencies Path Organization,1.3
+CAPTCHA Functionality,CAPTCHA Functionality,Implemented CAPTCHA reading (Plain text format image) functionality for Windows machine.,1.4
+Barcode Functionality,Barcode Functionality,Implemented Barcode reading capability,1.4
+OTP Integration,OTP Integration,Integrated one-time password (OTP) generation and verification.,1.4
+File Upload,File Upload,Implemented a method to upload files (attachments) to a web page.,1.4
+Zephyr Integration,Zephyr Enhancements,Enhanced Zephyr test execution by attaching log files for failed executions.,1.4
+Sauce Labs Integration,Sauce Labs Integration,Sauce Lab Enhancements,1.4
+API Enhancements,API Enhancements,Added Certificate Configuration for API Calls,1.4
+Test Automation,Test Automation Enhancements,Enhanced Parallel Thread Execution,1.4
+Test Automation,Test Automation Enhancements,Parallel Execution Enhancement,1.5
+Cross Browser Testing,Cross Browser Testing,Cross Browser Testing Enhancement,1.5
+Zephyr Integration,Zephyr Integration,Zephyr Enhancement - Field filtering in search Operation,1.5
+Sauce Labs Integration,Sauce Labs Integration,Sauce Lab Parallel Execution,1.5
+Zephyr Integration,Zephyr Integration,Initial integration.,1
+Zephyr Integration,Zephyr Integration,Upload failure attachments.,1
+Zephyr Integration,Zephyr Integration,Upload log files and existing test cycle execution.,1
+Zephyr Integration,Zephyr Integration,Zephyr test case creation.,1
+Zephyr Integration,Zephyr Integration,Zephyr naming convention.,1
+Zephyr Integration,Zephyr Integration,Zephyr code cleanup.,1
+Schema Validation,Schema Validation,Request schema validation method creation.,1
+Configuration Management,Configuration Management,Handling Gpath using YAML file.,1
+Configuration Management,Configuration Management,Configuration and reading values from CSV.,1
+Configuration Management,Configuration Management,Chrome driver temporary solution.,1
+Configuration Management,Configuration Management,DataHelper modifications based on review comments.,1
+Configuration Management,Configuration Management,Configuration structure change for rerunning failure scenarios.,1
+Configuration Management,Configuration Management,ProtectConfigFile to protect Config.yaml during code merge.,1
+Configuration Management,Configuration Management,Wait time configuration and element highlighter.,1
+Configuration Management,Configuration Management,Solution for driver changes.,1
+Dependency Management,Dependency Management,Nexus integration.,1
+Dependency Management,Dependency Management,Default profile added in POM.xml file.,1
+Configuration Management,Configuration Management,Code cleanup.,1
+API Enhancements,API Enhancements,API steps and utilities updates.,1
+API Enhancements,API Enhancements,API wrapper methods addition.,1
+API Enhancements,API Enhancements,Log info update with Report portal.,1
+API Enhancements,API Enhancements,Push log to Report portal.,1
+API Enhancements,API Enhancements,Password encryption updates.,1
+API Enhancements,API Enhancements,Fetch search results from Splunk.,1
+Test Automation,Test Automation Enhancements,GWT format validations.,1
+Test Automation,Test Automation Enhancements,Access modifiers updated.,1
+Test Automation,Test Automation Enhancements,Adding new steps for explicit wait.,1
+Test Automation,Test Automation Enhancements,Extent report and PDF report with timestamp.,1
+Test Automation,Test Automation Enhancements,Protect file plugin.,1
+Test Automation,Test Automation Enhancements,Retry failed scenarios.,1
+Mobile Testing,Mobile Testing,Mobile execution changes for iOS and Android.,1
+Test Automation,Test Automation Enhancements,Selenium-remote-driver dependency update.,1
+Reporting Enhancements,Reporting Enhancements,Modified extent report and PDF report with timestamp.,1
+Reporting Enhancements,Reporting Enhancements,Report structure changes.,1
+Reporting Enhancements,Reporting Enhancements,Spark report changes.,1
+Reporting Enhancements,Reporting Enhancements,Report plugin dependency issue fix.,1
+
+
+
+
+
+
+
+
 Area	Feature	version
 	Zephyr Integration (GBSCAF-4):	1.1
 	Schema Validation (GBSCAFDEV-17):	1.1
