66b68f810db84f2ae9aae80a05a437a8ab9cb099
manishdube <manish.dube@gmail.com>
2024-07-01 14:25:07 -0400

    Update loI.java

--- loI.java
+++ loI.java
@@ -1,1 +1,9 @@
 
+write me a letter of compliation violation based on the below:
+
+1. inapproriate and deliberate misleading of projects' viability for personal carrier growth and goals. 
+2. Deliberate opaque communications and reporting for upwards futher leading to misalignments in team.  
+3. Unresponsove leadership & management that carries a sense of entitlement and secreacy.  
+4. Absolute lack of any stndards of planning, process and protocola with projects. 
+5. Dismissive tone of offsite workers towards the lesser experienced female workers
+6.   
