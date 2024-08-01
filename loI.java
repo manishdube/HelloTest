53d418d6d03a026a503fc388d95d030f61a36d3e
manishdube <manish.dube@gmail.com>
2024-08-01 14:09:45 -0400

    Update loI.java

--- loI.java
+++ loI.java
@@ -1,134 +1,117 @@
 
-Feature Checklist
-Web
-Api
-DB - Oracle
-DB - DB2
-Saucelabs
-Zephyr
-Parallel Execution
-Cross broswer Execution
-Report Portal (initial)
-Karate
-Password Encryption
-Test Data handling - excel
-Test Data handling - Yml
-Gitlab runner setup - Windows
-barcode scanning
-CAPTCHA Functionality
-OTP mail thru UI
-Extent Report
-Splunk
-retrevial of Failure cases
-Failure screenshot
-Execution logs
--Appli tools
--Mobile 
-- Extent report (html, pdf)
-- Cucumber report
-- Video recording
-- Test data handling (csv)
 
-
-Write me a cover letter indicating my interest and expertise in enterprise wide performance optimizations for systems with strategies for D-day preo for 
- events such as black friday and amazon prime day as it pertains to online shopping. Do also honour the current employees of Macys who i have'd a chance to interact and spend hours with each of them individually understanding the tech stack and the tasks at hand. 
-  
-Write a short line thanking the below for the detailed discussions in the recent past.
-  Erics Newman
-  Ram Chenchu
-  Tami McCormack
-  Mark Kelly
-  Janine Dismer
-  Kia Holyfield
-  Rahul Baldawa
-  Isabelle Roderman
-  
-  
-
-describe peroformance tuning efforts in bullets for a resume based on the above requirements so the CV stands out.
-
-  Experity Health: 
-  The main elements of the tech stack that were performance tuned were:
-
-1. desktop based EMR system with dedicated citrix based health care data protection layers in place. 
-2. item 1 PROD infrastructure sizing optimization was performed which resulted in savings of over 600k,
-3. PODs were created that contained all the components of a vertical slice of the minimum viable product with all the relevant feautres 
-4. Sizing and capacity per component as it sits in the cloud services was conducted. performnace testing was condicted with measurements across the components to qualify the sizing.
-5. Tools such as jmeter, loader, flood.io, Artillery and several open source load testing platforms were utilised.
-6. Talk about the key metrics that were reviewed and used to make these measurements. Connections per sec, etc etc. 
-7. Talk about the iterative approach and how this perforamcen improvement strategy was implemented for monthly load bursts due to financial processign and usage ot the entire suite of applucations for complete processing. 
-  
-
-  Genesis Financial Systems:
-
-
-
-
-
-Software Engineer Lead - Platform and Performance 
-Duluth, GA · 1 week ago · 8 applicants
-On-siteMatches your job preferences, workplace type is On-site.  Full-timeMatches your job preferences, job type is Full-time.
-10,001+ employees · Retail
-77 school alumni work here
-8 of 10 skills match your profile - you may be a good fit
-See how you compare to 8 applicants. Retry Premium for $0
-
-
-Am I a good fit for this job?
-
-How can I best position myself for this job?
-
-Tell me more about Macy's
-
-
-Apply
-
-Save
-Save Software Engineer Lead - Platform and Performance  at Macy's
-Share
-Show more options
-About the job
-About
-
-Macy’s is proudly America’s Department Store. For more than 160 years, Macy’s has served generations at every stage of their lives. Customers come to us for fashion, value and celebration. Now is an exciting time to join Macy’s, Inc. The face of retail is changing, and change requires innovation.
-
-Macy’s Tech provides modern tools, platforms, and services to all parts of the business. Our team supports millions of customers in connected commerce across the technology hub at Macy’s Join our team to help shape the future of e-commerce and set the pace in retail technology. Whether focused on store technology, supply chain tech, application security, merchandising systems, or the mobile app – you’ll have opportunities to grow your career while finding meaningful ways to make a difference.
-
-Job Summary: We are looking for an experienced Performance Engineering Lead who will play a pivotal role in ensuring the optimal performance of our e-commerce platform on Google Cloud and microservices architecture. This role involves leading a team of performance engineers and coordinating holiday readiness testing for Macy's, ensuring that our platform can handle high traffic loads during peak seasons. Additionally, you will be responsible for implementing robust monitoring and alerting observability validation using tools such as Splunk, Dynatrace, and Stackdriver, in conjunction with performance testing tools like K6 and JMeter.
-
-Key Responsibilities
-
-Performance Testing Leadership: Lead and manage a team of performance engineers responsible for testing the scalability, performance, and availability of our e-commerce platform.
-Google Cloud Expertise: Leverage your expertise in Google Cloud to optimize platform performance and ensure efficient resource utilization.
-Spanner DB Performance: Collaborate with database administrators to monitor and optimize the performance of Spanner DB, ensuring data consistency and high availability.
-Microservices Architecture: Evaluate and fine-tune the performance of our microservices architecture to handle increased traffic loads.
-Holiday Readiness Testing: Develop and execute holiday readiness testing strategies and plans for Macy's, simulating high traffic scenarios to ensure a seamless shopping experience during peak seasons.
-Performance Tuning: Identify performance bottlenecks, conduct root cause analysis, and implement performance tuning strategies to enhance system responsiveness.
-Scalability Assessment: Assess the scalability of the platform, recommending infrastructure upgrades or optimizations as needed to accommodate growth.
-Monitoring and Alerting: Implement and manage robust monitoring and alerting solutions using tools like Splunk, Dynatrace, and Stackdriver to ensure real-time visibility into platform performance.
-Observability Validation: Validate observability practices to provide actionable insights into system behavior, helping to identify and address performance issues proactively.
-Load and Stress Testing: Design and execute load and stress testing scenarios using K6 and JMeter to validate the platform's ability to handle increased loads.
-Performance Metrics: Establish key performance indicators (KPIs) and metrics to measure, monitor, and report on platform performance.
-Documentation and Reporting: Maintain comprehensive documentation of performance test plans, results, monitoring configurations, and recommendations, providing regular reports to stakeholders.
-Collaboration: Collaborate closely with cross-functional teams, including development, quality assurance, and Macy's teams, to ensure alignment on performance goals and requirements.
-
-Qualifications
-
-Bachelor's degree in Computer Science, Software Engineering, or a related field; Master's degree preferred.
-We encourage candidates with either a bachelor’s degree or equivalent work experience in a related field to apply.
-5-8 years of experience in performance engineering, with a focus on e-commerce platforms.
-Cross channel performance experience is a plus, Site, Mobile, POS, Kiosk 
-End2End Customer journey performance scenario experience is required
-Expertise in Google Cloud services and cloud-based architectures.
-Strong knowledge of Spanner, Cassandra, Mongo and microservices architecture.
-Experience with performance testing tools such as K6 and JMeter.
-Proficiency in holiday readiness testing for e-commerce platforms.
-Hands-on experience with monitoring and alerting tools like Splunk, Dynatrace, and Stackdriver.
-Excellent leadership and team management skills.
-Strong problem-solving and analytical abilities.
-Excellent communication and collaboration skills.
-Data and Dashboarding skills are required.
-
-TECH00
-
-This position may be eligible for performance-based incentives/bonuses. Benefits include 401k, medical/vision/dental/life/disability insurance options, PTO accruals, Holidays, and more. Eligibility requirements may apply based on location, job level, classification, and length of employment. Additional benefit details are available at macysJOBS.com.
+GBSCAFDEV-542	Smoke scenario preparation for release	Task	IN PROGRESS	Wong, Allison (Berkeley Heights)	
+Actions
+ 	GBSCAFADT-50	Rerun Failed Scenarios in Cucumber	Story	DONE	Kannan, Gopalsamy (Chennai)	
+Actions
+ 	GBSCAFDEV-589	Validation of custom jar as dependency for GBSCAF framework	Task	DONE	S P, Kolunthuvelu (Chennai)	
+Actions
+ 	GBSCAFDEV-596	Chrome Browser issue fix - Windows	Task	DONE	DURAIKRISHNAN, S. (Chennai)	
+Actions
+ 	GBSCAFDEV-604	GBSCAF Release 1.2	Story	DONE	P, Jeeva (Chennai)	
+Actions
+GBSCAFDEV-608	Cucumber Parameterization for Test data yaml file	Task	IN PROGRESS	DURAIKRISHNAN, S. (Chennai)	
+Actions
+ 	GBSCAFDEV-616	Applitools Analysing	Task	DONE	DURAIKRISHNAN, S. (Chennai)	
+Actions
+ 	GBSCAFDEV-629	Supporting Documentation Team	Task	DONE	P, Jeeva (Chennai)	
+Actions
+ 	GBSCAFDEV-651	Adding Select Class methods and its stepdef	Task	DONE	Kannan, Gopalsamy (Chennai)	
+Actions
+ 	GBSCAFDEV-654	Proxy configuration for Zephyr	Task	DONE	P, Jeeva (Chennai)	
+Actions
+ 	GBSCAFDEV-671	API Logs in GBSCAF	Task	DONE	DURAIKRISHNAN, S. (Chennai)	
+Actions
+ 	GBSCAFDEV-680	Zephyr Enhancements	Task	DONE	P, Jeeva (Chennai)	
+Actions
+ 	GBSCAFDEV-704	Supporting GBSCAF Documentation Team	Task	DONE	DURAIKRISHNAN, S. (Chennai)	
+Actions
+ 	GBSCAFDEV-710	GQS Xml framework integration	Task	DONE	P, Jeeva (Chennai)	
+Actions
+ 	GBSCAFDEV-728	Create steps to get OTP from mail using selenium	Story	DONE	S P, Kolunthuvelu (Chennai)	
+Actions
+ 	GBSCAFDEV-734	File uploading method to push files to a Web page	Story	DONE	S P, Kolunthuvelu (Chennai)	
+Actions
+GBSCAFDEV-736	Failure Screen Shot Renaming	Task	ON-HOLD	DURAIKRISHNAN, S. (Chennai)	
+Actions
+GBSCAFDEV-738	Avoid Overloading Reports Memory	Task	TO DO	DURAIKRISHNAN, S. (Chennai)	
+Actions
+GBSCAFDEV-739	Module wise test cycle structure in Zephyr	Task	READY	P, Jeeva (Chennai)	
+Actions
+ 	GBSCAFDEV-760	Sauce Lab Updates	Task	DONE	DURAIKRISHNAN, S. (Chennai)	
+Actions
+GBSCAFDEV-762	Zephyr integration with GQSXML framework	Task	IN PROGRESS	P, Jeeva (Chennai)	
+Actions
+ 	GBSCAFDEV-766	Saucelab enhancements - Test case name, scenario name for test runs in Saucelab	Story	DONE	Dube, Manish (Alpharetta)	
+Actions
+GBSCAFDEV-769	PDF Validation	Task	TO DO	DURAIKRISHNAN, S. (Chennai)	
+Actions
+ 	GBSCAFDEV-782	Capcha Reading	Story	DONE	Kannan, Gopalsamy (Chennai)	
+Actions
+ 	GBSCAFDEV-785	Barcode Reading	Story	DONE	Kannan, Gopalsamy (Chennai)	
+Actions
+ 	GBSCAFDEV-797	Adding Certificate Configuration while doing API call	Story	DONE	S P, Kolunthuvelu (Chennai)	
+Actions
+ 	GBSCAFRD-58	JDK compatibility confirmation and related dependencies clean up. 	Task	DONE	Dube, Manish (Alpharetta)	
+Actions
+ 	GBSCAFDEV-810	GBSCAF Parallel Execution issue fix	Task	DONE	DURAIKRISHNAN, S. (Chennai)	
+Actions
+GBSCAFDEV-823	Excel validation utils	Task	READY	S P, Kolunthuvelu (Chennai)	
+Actions
+GBSCAFDEV-824	Disable save password popup on Chrome & Edge	Task	TO DO	Kannan, Gopalsamy (Chennai)	
+Actions
+ 	GBSCAFDEV-830	Parallel Execution in GBSCAF	Task	DONE	DURAIKRISHNAN, S. (Chennai)	
+Actions
+GBSCAFDEV-833	Report Portal - API	Task	TO DO	DURAIKRISHNAN, S. (Chennai)	
+Actions
+GBSCAFDEV-839	PingId Automation	Task	ON-HOLD	S P, Kolunthuvelu (Chennai)	
+Actions
+ 	GBSCAFDEV-859	Cross Browser Testing	Story	DONE	DURAIKRISHNAN, S. (Chennai)	
+Actions
+ 	GBSCAFDEV-861	Zephyr Enhancement - Field filtering in search Operation	Task	DONE	P, Jeeva (Chennai)	
+Actions
+GBSCAFDEV-866	GBSCAF - API Enhancement	Story	IN PROGRESS	DURAIKRISHNAN, S. (Chennai)	
+Actions
+ 	GBSCAFDEV-867	FDPOS Database - Parallel Execution	Task	DONE	P, Jeeva (Chennai)	
+Actions
+ 	GBSCAFADT-319	GBSCAF - Selenium Upgrade 4.22.0	Story	DONE	Kannan, Gopalsamy (Chennai)	
+Actions
+GBSCAFDEV-881	Database Enhancements	Story	IN PROGRESS	S P, Kolunthuvelu (Chennai)	
+Actions
+ 	GBSCAFDEV-890	Adding deselectAll method to support multiselect Select box	Task	DONE	Kannan, Gopalsamy (Chennai)	
+Actions
+ 	GBSCAFDEV-891	API Parallel Execution GBSCAF	Story	DONE	DURAIKRISHNAN, S. (Chennai)	
+Actions
+ 	GBSCAFDEV-895	api enhancements - develop more operations support	Story	CANCELLED	DURAIKRISHNAN, S. (Chennai)	
+Actions
+ 	GBSCAFDEV-896	parallel execution reporting enhancements	Story	DONE	P, Jeeva (Chennai)	
+Actions
+GBSCAFDEV-908	Database Parallel Execution	Story	IN PROGRESS	P, Jeeva (Chennai)	
+Actions
+ 	GBSCAFDEV-909	Adding Logs or Exceptions in Zephyr Utility	Story	CANCELLED	P, Jeeva (Chennai)	
+Actions
+GBSCAFDEV-910	Report Portal POC	Story	ON-HOLD	DURAIKRISHNAN, S. (Chennai)	
+Actions
+GBSCAFDEV-911	Dependency Cleanup	Story	PLANNING	Dube, Manish (Alpharetta)	
+Actions
+ 	GBSCAFADT-336	Database connectivity issue	Task	DONE	S P, Kolunthuvelu (Chennai)	
+Actions
+GBSCAFDEV-938	GBSCAF Framework Enhancement	Task	TO DO	Unassigned	
+Actions
+GBSCAFDEV-943	chrome switch to new tab	Story	IN PROGRESS	S P, Kolunthuvelu (Chennai)	
+Actions
+GBSCAFADT-355	develop faster handling of Page objects	Task	TO DO	Unassigned	
+Actions
+GBSCAFADT-364	Parallel Executions - multiple user / multiple scenarios	Story	TO DO	Unassigned	
+Actions
+GBSCAFADT-365	Transfer files to Saucelabs during execution	Story	TO DO	Unassigned	
+Actions
+GBSCAFADT-366	Establish and Maintain Snowflake Connectivities during executions	Story	TO DO	Unassigned	
+Actions
+GBSCAFADT-371	Clone excel template - download , fill, upload	Story	TO DO	Unassigned	
+Actions
+GBSCAFADT-373	retrieve data from Transport layer for use in automation test flow.	Story	TO DO	Unassigned	
+Actions
+GBSCAFADT-374	method for retrieving values from dynamic web elements	Story	TO DO	Unassigned	
+Actions
+GBSCAFADT-375	Screen scrape data from UI in excel, json during executions	Story	TO DO	Unassigned
